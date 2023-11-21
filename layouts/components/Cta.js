85c4e44a71fab232096423a1b9b50dc70c3a4b6b
Shaan Alam <shaanalam369@gmail.com>
2023-11-21 19:19:57 +0530

    changed cta image

--- layouts/components/Cta.js
+++ layouts/components/Cta.js
@@ -4,18 +4,13 @@ function Cta() {
   return (
     <section className="section relative mb-10 overflow-hidden bg-black/95">
       <img
-        src="./images/3d.webp"
+        src="./images/3dperson.png"
         alt=""
-        className="absolute right-0 top-0 rotate-180 opacity-10 md:opacity-20"
-      />
-      <img
-        src="./images/3dperson.webp"
-        alt=""
-        className="absolute hidden xl:block right-[20%] top-10 shadow-xl"
+        className="absolute right-[10%] top-4 hidden w-[25%] shadow-xl xl:block"
       />
       <div className="container">
         <h1 className="text-white">Let&apos;s connet together</h1>
-        <p className="my-2 w-full md:w-1/2 text-gray-200">
+        <p className="my-2 w-full text-gray-200 md:w-1/2">
           Ready to enhance your online presence? Whether it's a specific
           project, a quote, or exploring how we can boost your digital success,
           contact us now to begin.
